4e21370ddc1aed4170fef4448591e21b342e9c26
first file <shunli.gao@zhaopin.com.cn>
2019-12-11 17:17:01 +0800

    create lucene analysis git repository

---
+++ src/test/java/org/apache/lucene/query/TermQueryTester.java
@@ -0,0 +1,72 @@
+package org.apache.lucene.query;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.FieldType;
+import org.apache.lucene.document.PayloadAnalyzer;
+import org.apache.lucene.index.*;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
+
+public class TermQueryTester {
+
+    @Before
+    public void before() throws IOException {
+        FileUtils.forceMkdir(new File("/data/logs/lucene"));
+    }
+
+    @Test
+    public void test() throws IOException {
+        IndexWriterConfig config = new IndexWriterConfig();
+        config.setUseCompoundFile(false);
+        Directory dir = FSDirectory.open(Paths.get("/data/logs/lucene"));
+        IndexWriter writer = new IndexWriter(dir, config);
+        PayloadAnalyzer analyzer = new PayloadAnalyzer();
+        analyzer.setPayloadData("content", "hi".getBytes(StandardCharsets.UTF_8), 0, 2);
+        FieldType type = new FieldType();
+        type.setStored(true);
+        type.setTokenized(true);
+        type.setIndexOptions(IndexOptions.DOCS_AND_FREQS_AND_POSITIONS_AND_OFFSETS);
+        type.freeze();
+        //
+        Document doc = new Document();
+        doc.add(new Field("content", "one", type));
+        writer.addDocument(doc);
+        //
+        doc = new Document();
+        doc.add(new Field("content", "two", type));
+        writer.addDocument(doc);
+        //
+        doc = new Document();
+        doc.add(new Field("content", "three", type));
+        writer.addDocument(doc);
+
+        writer.commit();
+
+        IndexReader reader = DirectoryReader.open(dir);
+        IndexSearcher searcher = new IndexSearcher(reader);
+        TopDocs docs = searcher.search(new TermQuery(new Term("content", "two")), 10);
+        Assert.assertEquals(docs.totalHits, 1);
+        reader.close();
+        writer.close();
+    }
+
+    @After
+    public void after() throws IOException {
+        FileUtils.deleteDirectory(new File("/data/logs/lucene"));
+    }
+
+}
