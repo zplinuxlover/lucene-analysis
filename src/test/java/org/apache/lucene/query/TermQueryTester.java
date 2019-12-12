86998af425ec15b80490574850d023ca8a8bda90
first file <shunli.gao@zhaopin.com.cn>
2019-12-12 15:16:29 +0800

    create lucene analysis git repository

--- src/test/java/org/apache/lucene/query/TermQueryTester.java
+++ src/test/java/org/apache/lucene/query/TermQueryTester.java
@@ -30,19 +30,24 @@ public class TermQueryTester {
 
     @Test
     public void test() throws IOException {
-        IndexWriterConfig config = new IndexWriterConfig();
-        config.setUseCompoundFile(false);
         Directory dir = FSDirectory.open(Paths.get("/data/logs/lucene"));
-        IndexWriter writer = new IndexWriter(dir, config);
         PayloadAnalyzer analyzer = new PayloadAnalyzer();
         analyzer.setPayloadData("content", "hi".getBytes(StandardCharsets.UTF_8), 0, 2);
+        IndexWriterConfig config = new IndexWriterConfig(analyzer);
+        config.setUseCompoundFile(false);
+        IndexWriter writer = new IndexWriter(dir, config);
+        //
         FieldType type = new FieldType();
         type.setStored(true);
         type.setTokenized(true);
         type.setIndexOptions(IndexOptions.DOCS_AND_FREQS_AND_POSITIONS_AND_OFFSETS);
+        type.setStoreTermVectors(true);
+        type.setStoreTermVectorPositions(true);
+        type.setStoreTermVectorOffsets(true);
         type.freeze();
+        //
         Document doc;
-
+        //
         doc = new Document();
         doc.add(new Field("content", "one", type));
         writer.addDocument(doc);
@@ -50,19 +55,19 @@ public class TermQueryTester {
         doc = new Document();
         doc.add(new Field("content", "two", type));
         writer.addDocument(doc);
-
+        //
         doc = new Document();
         doc.add(new Field("content", "three", type));
         writer.addDocument(doc);
-
+        //
         writer.commit();
-
+        //
         IndexReader reader = DirectoryReader.open(dir);
         IndexSearcher searcher = new IndexSearcher(reader);
         TopDocs docs = searcher.search(new TermQuery(new Term("content", "two")), 10);
         Assert.assertEquals(docs.totalHits, 1);
         reader.close();
-
+        //
         writer.close();
     }
 
